c526394ce4c4a2023ba467961d11ec088d145409
MokiEng <meconen20@gmail.com>
2022-12-08 14:07:40 -0800

    size of data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,8 +4,9 @@
 *
 *Return: Always 0 (Success)
 */
-{
+
 int main(void)
+{
 printf("size of a char: %i byte(s)\n", sizeof(char));
 printf("size of an int: %i byte(s)\n", sizeof(int));
 printf("size of a long int: %i byte(s)\n", sizeof(long int));
