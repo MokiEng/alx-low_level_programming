4e8b9f3073a8291f92369b9421c269c2ca3e051e
MokiEng <meconen20@gmail.com>
2022-12-09 11:15:49 -0800

    print tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+char c;
+
+c = 'z';
+while
+(c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
