f37000e97f26ee9e86ccae222c436f8ee31641df
MokiEng <meconen20@gmail.com>
2022-12-09 09:51:51 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*more headers goes there */
+/**
+*main - Entry point
+* 
+*Return: Always 0 (Success)
+*/
+/*betty style doc for function main goes there */
+
+int main(void)
+{
+char c;
+
+c = 'a';
+
+while
+(c <= 'z')
+{
+putchar(c);
+c++;
+}
+
+putchar('\n');
+return (0);
+}
