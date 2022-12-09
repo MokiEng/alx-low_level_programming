465f938eb1e28f95623771754059188a1e1ea0c2
MokiEng <meconen20@gmail.com>
2022-12-09 10:38:32 -0800

    new print alpahbet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,11 +4,10 @@
 /*more headers goes there */
 /**
 *main - Entry point
-* 
-*Return: Always 0 (Success)
+*
+* Return: Always 0 (Success)
 */
 /*betty style doc for function main goes there */
-
 int main(void)
 {
 char c;
@@ -17,10 +16,10 @@ c = 'a';
 while
 (c <= 'z')
 {
+if ((c != 'q' && c != 'e') && c <= 'z')
 putchar(c);
 c++;
 }
-
 putchar('\n');
 return (0);
 }
