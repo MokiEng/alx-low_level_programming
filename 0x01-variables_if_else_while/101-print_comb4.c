2a5a5cdcc0d54be6164038e7cd18fe53acad16cb
MokiEng <meconen20@gmail.com>
2022-12-09 14:26:09 -0800

    task11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,16 +14,17 @@ int main(void)
 int ones = '0';
 int tens = '0';
 int hundreds = '0';
-	
+
 for (hundreds = '0'; hundreds <= '9'; hundreds++)
 {
 
 for (tens = '0'; tens <= '9'; tens++)
+
 {
 
 for (ones = '0'; ones <= '9'; ones++)
 {
-if (!((ones == tens) || (tens == hundreds) || 
+if (!((ones == tens) || (tens == hundreds) ||
 			(tens > ones) || (hundreds > tens)))
 {
 
@@ -41,7 +42,6 @@ putchar(' ');
 
 }
 }
-
 }
 }
 }
