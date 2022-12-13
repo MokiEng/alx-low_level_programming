4590db0ff3d77dd3dc5706921040fec553569936
MokiEng <meconen20@gmail.com>
2022-12-13 09:47:15 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_alphabet - prints the alpha in lower case
+*
+*/
+void print_alphabet(void)
+
+{
+
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+
+_putchar('\n');
+
+}
