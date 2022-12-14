a9d18c0af1b9f72cb07f22b0654aad657e3dc35a
MokiEng <meconen20@gmail.com>
2022-12-14 12:36:34 -0800

    time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,38 +2,50 @@
 #include <stdio.h>
 /**
 *print_times_table -  function that prints the multiplication table
-* @n: user value
+*@n: user value
+*
+*Return: number matrix
 */
 void print_times_table(int n)
 {
 	int num, mul, prod;
 
-	if (n >= 0 && n <= 15)
+	if (n >= 0 && n <= 14)
 	{
 		for (num = 0; num <= n; num++)
 		{
-			_putchar('0');
-			for (mul = 1; mul <= n; mul++)
+			for (mul = 0; mul <= n; mul++)
 			{
-				_putchar(',');
-				_putchar(' ');
 				prod = num * mul;
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod >= 100)
+				if (prod > 99)
 				{
+					_putchar(',');
+					_putchar(32);
 					_putchar((prod / 100) + '0');
-					_putchar((prod / 100) % 10 + '0');
+					_putchar(((prod / 10) % 10) + '0');
+					_putchar((prod % 10) + '0');
 				}
-				else if (prod <= 99 && prod >= 10)
+				else if (prod > 9)
 				{
-					_putchar((prod / 10) + '0');
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((prod / 10) % 10) + '0');
+					_putchar((prod % 10) + '0');
 				}
-				_putchar((prod % 10) + '0');
-			}
-			_putchar('\n');
+				else
+				{
+					if (mul != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(prod + '0');
+				}
+				}
+				_putchar('\n');
 		}
 	}
 }
