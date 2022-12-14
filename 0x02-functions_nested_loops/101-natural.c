460cd719efd647d9305d90e73070ea1d8ad08ab3
MokiEng <meconen20@gmail.com>
2022-12-14 11:02:20 -0800

    task13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main -  prints the sum of all mul of 3 or 5 upto 1024
+*Return: Always (success)
+*/
+int main(void)
+{
+int i, z;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+		i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
