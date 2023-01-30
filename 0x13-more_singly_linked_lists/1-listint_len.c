921065d5d1566dc8fb75736095406f4092469d15
MokiEng <meconen20@gmail.com>
2023-01-30 13:19:44 -0800

    list length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * listint_len - Calculate the number of elements.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *tmp;
+	unsigned int counter = 0;
+
+	tmp = h;
+	while (tmp)
+	{
+		counter++;
+		tmp = tmp->next;
+	}
+	return (counter);
+}
