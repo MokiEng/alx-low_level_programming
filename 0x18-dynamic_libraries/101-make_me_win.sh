9dde1005076e7e7948230d7d739074a1353f89ea
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-03-21 00:54:03 -0700

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/MokiEng/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD="$PWD/../libtest.so"
+wget -P /tmp https://github.com/MokiEng/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD=/tmp/libtest.so
