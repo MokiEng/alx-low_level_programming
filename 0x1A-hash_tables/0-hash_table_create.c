ca153f48bd2f6713f67de31dd6a5e89e8cb0bdb6
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-04-06 13:09:40 -0700

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
+
 /**
- * *hash_table_create - Creates a hash table.
+ * hash_table_create - Creates a hash table.
  * @size: The size of the array .
  * Return : If ana error occure - NULL.
  *          Otherwise - a pointer to the new hash table
- * Description: a function that creates a hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
