84ef6fa1dd8141737246aa9c2f3e5f44a178bd7c
MokiEng <meconen20@gmail.com>
2023-04-07 16:24:51 -0700

    ht{}

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,13 +9,19 @@
  *       written in the 0x14-file_io directory.
  */
 
-#include <sys/types.h>
+#include <elf.h>
 
-#include <sys/stat.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <unistd.h>
 
 #include <fcntl.h>
 
-#include <unistd.h>
+#include <sys/types.h>
+
+#include <sys/stat.h>
 
 
 
@@ -25,6 +31,34 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
+ssize_t read_textfile(const char *filename, size_t letters);
 
+int create_file(const char *filename, char *text_content);
+
+int append_text_to_file(const char *filename, char *text_content);
+
+/**
+ * ELF header functions
+ */
+
+void check_elf(unsigned char *e_ident);
+
+void print_magic(unsigned char *e_ident);
+
+void print_class(unsigned char *e_ident);
+
+void print_data(unsigned char *e_ident);
+
+void print_version(unsigned char *e_ident);
+
+void print_abi(unsigned char *e_ident);
+
+void print_osabi(unsigned char *e_ident);
+
+void print_type(unsigned int e_type, unsigned char *e_ident);
+
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+
+void close_elf(int elf);
 
 #endif /* MAIN_H */
