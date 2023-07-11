235466f9458e4a124a7a167ffe40aaad8fc3567c
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-07-11 00:48:16 -0700

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,7 +7,7 @@
  * @array: a pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
- * Return : -1 If value is not present in array
+ * Return: -1 If value is not present in array
  */
 int linear_search(int *array, size_t size, int value)
 {
