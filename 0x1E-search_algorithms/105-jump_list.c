12a902dfcd825cb33c56ca988e4e421822444c02
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-07-12 12:58:34 -0700

    Update 105-jump_list.c

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -32,7 +32,7 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		while (list->next && list->index< index)
 			list = list->next;
 
-		if (list->next == NULL && current != list->index)
+		if (list->next == NULL && index != list->index)
 			index = list->index;
 
 		printf("Value checked at index [%d] = [%d]\n", (int)index, list->n);
